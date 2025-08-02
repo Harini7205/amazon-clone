1b22353a04524c70b47a1d21fabfdacd481562ef
Harini <87068879+Harini7205@users.noreply.github.com>
2025-08-02 19:36:25 +0530

    Update firebase.js

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -1,17 +1,19 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
+
 const firebaseConfig = {
-    apiKey: "AIzaSyAsshddjeDUwP_5Aa9U9a-L54ok_bTuM-c",
-    authDomain: "clone-fa401.firebaseapp.com",
-    projectId: "clone-fa401",
-    storageBucket: "clone-fa401.appspot.com",
-    messagingSenderId: "1047799706289",
-    appId: "1:1047799706289:web:75a26402a21beff401a890",
-    measurementId: "G-V047F18X0G"
-  };
-const firebaseApp=initializeApp(firebaseConfig);
-const db=getFirestore(firebaseApp);
-const auth=getAuth(firebaseApp);
-export {db,auth};
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID
+};
+
+const firebaseApp = initializeApp(firebaseConfig);
+const db = getFirestore(firebaseApp);
+const auth = getAuth(firebaseApp);
+
+export { db, auth };
